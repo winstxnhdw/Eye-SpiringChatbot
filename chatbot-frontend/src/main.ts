c9498c7eaf95b7c41a258902b6e55a9563129d08
winstxnhdw <winstxnhdw@gmail.com>
2022-02-12 16:42:21 +0800

    remove useless boilerplate

--- chatbot-frontend/src/main.ts
+++ chatbot-frontend/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.provide('$axios', axios)
+app.mount('#app')
